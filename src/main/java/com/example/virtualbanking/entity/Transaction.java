bbb888f7e38a95daae3ff3f94a94039b4bd8e42f
khush404 <ckhushboo483@gamil.com>
2026-09-01 19:22:14 +0530

    Add transaction entity

---
+++ src/main/java/com/example/virtualbanking/entity/Transaction.java
@@ -0,0 +1,65 @@
+package com.example.virtualbanking.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    private String type;
+
+    private LocalDateTime timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    public Transaction(){
+
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public BigDecimal getAmount(){
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount){
+        this.amount = amount;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public LocalDateTime getTimestamp(){
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp){
+        this.timestamp = timestamp;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account){
+        this.account = account;
+    }
+}
