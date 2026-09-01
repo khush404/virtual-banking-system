3d29605a358e093a029956e5e5f710f7e96194bf
khush404 <ckhushboo483@gmail.com>
2026-09-01 20:13:16 +0530

    blah

--- src/main/java/com/example/virtualbanking/dto/LoginRequest.java
+++ src/main/java/com/example/virtualbanking/dto/LoginRequest.java
@@ -4,11 +4,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class LoginRequest {
-    @NotBlank(message = "Email is required")
+    @NotBlank(message = "Email is required  ")
     @Email(message = "Enter a valid email")
     private String email;
 
-    @NotBlank(message = "Password is required")
+    @NotBlank(message = "Password is required  ")
     private String password;
 
     public LoginRequest(){
